0371121c15c910b8a35829574f302486a07d90f0
Andrea Quintero <andreaquintero00@outlook.com>
2023-09-19 16:09:45 -0700

    courseinfo step 1

---
+++ part1/courseinfo/src/components/Content.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Content = ({ parts }) => {
+    return (
+        <>
+            {
+                parts.map((part, index) => (
+                    <p key={index}>{part.name} {part.exercises}</p>
+                ))
+            }
+        </>
+    )
+}
+
+export default Content
