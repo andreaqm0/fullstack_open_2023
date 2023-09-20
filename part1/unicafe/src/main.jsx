75c26e36b393e4f08fa05dcc6985ae694d832473
Andrea Quintero <andreaquintero00@outlook.com>
2023-09-19 17:04:05 -0700

    step 5

--- part1/unicafe/src/main.jsx
+++ part1/unicafe/src/main.jsx
@@ -7,13 +7,16 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   const getTotal = () => good + neutral + bad
+  const addGoodFeedback = () => setGood(good + 1)
+  const addNeutralFeedback = () => setNeutral(neutral + 1)
+  const addBadFeedback = () => setBad(bad + 1)
 
   return (
     <>
       <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button action={addGoodFeedback} text="good" />
+      <Button action={addNeutralFeedback} text="neutral" />
+      <Button action={addBadFeedback} text="bad" />
       {
         getTotal() > 0 
           ? <Statistics good={good} neutral={neutral} bad={bad} total={getTotal()} />
@@ -27,13 +30,25 @@ const Statistics = ({ good, neutral, bad, total }) => {
   return (
     <>
       <h2>Statistics</h2>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {total}</div>
-      <div>average {((good - bad) / (total)) || 0}</div>
-      <div>positive {(good / total) || 0}%</div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={((good - bad) / (total)) || 0} />
+      <StatisticLine text="positive" value={((good / total) || 0) + '%'} />
     </>
+  )
+}
+
+const Button = ({text, action}) => {
+  return (
+    <button onClick={action}>{text}</button>
+  )
+}
+
+const StatisticLine  = ({text, value}) => {
+  return (
+    <div>{text} {value}</div>
   )
 }
 
